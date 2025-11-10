7c4ff80dc87650cb0eee992ebcde115a8afd7158
Maxim Shmelov <mokinous@gmail.com>
2025-11-10 14:25:34 +0000

    update schema 'localhost' -> '192.168.0.225'

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(requestLogger);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:8081'], // web + expo dev
+    origin: ['http://192.168.0.225:5173' /*, 'http://localhost:8081'*/], // web + expo dev
     credentials: true,
   }),
 );
